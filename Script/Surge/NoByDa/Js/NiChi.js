1da8a8d96b82a57c020d686de267fc45e55d23a7
peng-Lx <98009929+PF2022@users.noreply.github.com>
2022-07-22 10:33:42 +0800

    Update NiChi.js

--- Script/Surge/NoByDa/Js/NiChi.js
+++ Script/Surge/NoByDa/Js/NiChi.js
@@ -3,13 +3,13 @@ NiChi 解锁素材包
 ***************************
 QuantumultX:
 [rewrite_local]
-^https?:\/\/m(p|ini-hk)\.bybutter\.com\/mood\/(official-templates|privileges) url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/NiChi.js
+^https?:\/\/m(p|ini-hk)\.bybutter\.com\/mood\/(official-templates|privileges) url script-response-body https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/NiChi.js
 [mitm]
 hostname = m*.bybutter.com
 ***************************
 Surge4 or Loon:
 [Script]
-http-response https?:\/\/m(p|ini-hk)\.bybutter\.com\/mood\/(official-templates|privileges) requires-body=1,max-size=524288,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/NiChi.js
+http-response https?:\/\/m(p|ini-hk)\.bybutter\.com\/mood\/(official-templates|privileges) requires-body=1,max-size=524288,script-path=https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/NiChi.js
 [MITM]
 hostname = m*.bybutter.com
 **************************/
