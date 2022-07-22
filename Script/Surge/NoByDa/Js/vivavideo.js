bd9c59401dcf097f18c96b8f495f9d125bd6dc0d
peng-Lx <98009929+PF2022@users.noreply.github.com>
2022-07-22 10:03:53 +0800

    Update vivavideo.js

--- Script/Surge/NoByDa/Js/vivavideo.js
+++ Script/Surge/NoByDa/Js/vivavideo.js
@@ -3,13 +3,13 @@
 ***************************
 QuantumultX:
 [rewrite_local]
-^https:\/\/(xy-viva\.kakalili|api-chn.rthdo)\.com\/api\/rest\/u\/vipVerifyReceipt url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
+^https:\/\/(xy-viva\.kakalili|api-chn.rthdo)\.com\/api\/rest\/u\/vipVerifyReceipt url script-response-body https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/vivavideo.js
 [mitm]
 hostname = xy-viva.kakalili.com, api-chn.rthdo.com
 ***************************
 Surge4 or Loon:
 [Script]
-http-response ^https:\/\/(xy-viva\.kakalili|api-chn.rthdo)\.com\/api\/rest\/u\/vipVerifyReceipt requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/vivavideo.js
+http-response ^https:\/\/(xy-viva\.kakalili|api-chn.rthdo)\.com\/api\/rest\/u\/vipVerifyReceipt requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/vivavideo.js
 [MITM]
 hostname = xy-viva.kakalili.com, api-chn.rthdo.com
 **************************/
