ebdb07fe6d308bd11535e7d68d94e74907227f43
peng-Lx <98009929+PF2022@users.noreply.github.com>
2022-07-22 10:38:42 +0800

    Update jibjab.js

--- Script/Surge/NoByDa/Js/jibjab.js
+++ Script/Surge/NoByDa/Js/jibjab.js
@@ -3,13 +3,13 @@ JibJab 解锁高级功能
 ***************************
 QuantumultX:
 [rewrite_local]
-https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/jibjab.js
+https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user url script-response-body https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/jibjab.js
 [mitm]
 hostname = origin-prod-phoenix.jibjab.com
 ***************************
 Surge4 or Loon:
 [Script]
-http-response https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/jibjab.js
+http-response https:\/\/origin-prod-phoenix\.jibjab\.com\/v1\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/jibjab.js
 [MITM]
 hostname = origin-prod-phoenix.jibjab.com
 **************************/
