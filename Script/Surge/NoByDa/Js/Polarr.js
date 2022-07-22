d44c1d3474383e88d39dbf8604a8d33cf12f1a76
peng-Lx <98009929+PF2022@users.noreply.github.com>
2022-07-22 10:36:01 +0800

    Update Polarr.js

--- Script/Surge/NoByDa/Js/Polarr.js
+++ Script/Surge/NoByDa/Js/Polarr.js
@@ -3,13 +3,13 @@ Polarr泼辣修图 解锁特权 (需恢复购买)
 ***************************
 QuantumultX:
 [rewrite_local]
-^https:\/\/api\.polaxiong\.com\/v1\/payments\/appleiap\/receipts\/confirmation url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Polarr.js
+^https:\/\/api\.polaxiong\.com\/v1\/payments\/appleiap\/receipts\/confirmation url script-response-body https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/Polarr.js
 [mitm]
 hostname = api.polaxiong.com
 ***************************
 Surge4 or Loon:
 [Script]
-http-response ^https:\/\/api\.polaxiong\.com\/v1\/payments\/appleiap\/receipts\/confirmation requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Polarr.js
+http-response ^https:\/\/api\.polaxiong\.com\/v1\/payments\/appleiap\/receipts\/confirmation requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/Polarr.js
 [MITM]
 hostname = api.polaxiong.com
 **************************/
