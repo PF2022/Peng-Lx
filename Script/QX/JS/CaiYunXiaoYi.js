cfe0c6ceff09530f8ee2d4ed0d3808dfe4c9e27b
peng-Lx <98009929+PF2022@users.noreply.github.com>
2022-07-22 22:30:54 +0800

    Update CaiYunXiaoYi.js

--- Script/QX/JS/CaiYunXiaoYi.js
+++ Script/QX/JS/CaiYunXiaoYi.js
@@ -4,8 +4,8 @@ QX
 [rewrite_local]
 ^https:\/\/api\.interpreter\.caiyunai\.com\/v1\/user url script-response-body https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/QX/JS/CaiYunXiaoYi.js
 hostname = api.interpreter.caiyunai.com
-Surge
-***************************
+**************************
+surge&loon
 [Script]
 http-response ^https:\/\/api\.interpreter\.caiyunai\.com\/v1\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/QX/JS/CaiYunXiaoYi.js
 hostname = api.interpreter.caiyunai.com
