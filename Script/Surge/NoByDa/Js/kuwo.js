6699a372f843735a6775b552e659e409b1a8ffce
peng-Lx <98009929+PF2022@users.noreply.github.com>
2022-07-22 15:18:10 +0800

    Update kuwo.js

--- Script/Surge/NoByDa/Js/kuwo.js
+++ Script/Surge/NoByDa/Js/kuwo.js
@@ -1,18 +1,19 @@
 /*
 酷我音乐 解锁会员试听及部分功能
-***************************
+************************
 QuantumultX:
 [rewrite_local]
 ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) url script-response-body https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/kuwo.js
 [mitm]
 hostname = vip1.kuwo.cn
-***************************
+************************
 Surge4 or Loon:
 [Script]
 http-response ^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/user\/vip|vip\/spi/mservice) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/kuwo.js
 [MITM]
 hostname = vip1.kuwo.cn
-**************************/
+************************
+*/
 
 var body = $response.body;
 var url = $request.url;
