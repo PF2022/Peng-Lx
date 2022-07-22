d4c1635238d6b2050e86ff3d7a99833ee694794c
peng-Lx <98009929+PF2022@users.noreply.github.com>
2022-07-22 10:31:20 +0800

    Update CamScanner.js

--- Script/Surge/NoByDa/Js/CamScanner.js
+++ Script/Surge/NoByDa/Js/CamScanner.js
@@ -3,19 +3,13 @@ CamScanner 解锁部分高级特权
 ***************************
 Quantumult X:
 [rewrite_local]
-^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/CamScanner.js
 [mitm]
 hostname = ap*.intsig.net
 ***************************
 Surge4 or Loon:
 [Script]
-http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
-[MITM]
-hostname = ap*.intsig.net
-***************************
-Quantumult:  
-[REWRITE]
-https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url simple-response SFRUUC8xLjEgMjAwIE9LCgp7CiAiZGF0YSI6IHsKICAicHNubF92aXBfcHJvcGVydHkiOiB7CiAgICJleHBpcnkiOiAiMTY0MzczMTIwMCIKICB9CiB9Cn0=
+http-response https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/NoByDa/Js/CamScanner.js
 [MITM]
 hostname = ap*.intsig.net
 **************************/
