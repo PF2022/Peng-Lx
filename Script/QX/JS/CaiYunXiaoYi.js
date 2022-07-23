94df08d7689ceb9d20ace8cbcf931a385cf6ecb8
peng-Lx <98009929+PF2022@users.noreply.github.com>
2022-07-23 10:42:48 +0800

    Update CaiYunXiaoYi.js

--- Script/QX/JS/CaiYunXiaoYi.js
+++ Script/QX/JS/CaiYunXiaoYi.js
@@ -3,12 +3,12 @@
 QX
 [rewrite_local]
 ^https:\/\/api\.interpreter\.caiyunai\.com\/v1\/user url script-response-body https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/QX/JS/CaiYunXiaoYi.js
+**************************
 hostname = api.interpreter.caiyunai.com
 **************************
 surge&loon
 [Script]
 http-response ^https:\/\/api\.interpreter\.caiyunai\.com\/v1\/user requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/QX/JS/CaiYunXiaoYi.js
-hostname = api.interpreter.caiyunai.com
 */
 
 var obj = JSON.parse($response.body);
