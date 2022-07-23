b6df82fe427a8ad0c8cf06a0efe84203d71bfda0
peng-Lx <98009929+PF2022@users.noreply.github.com>
2022-07-23 11:55:50 +0800

    Update kekeyingyu.js

--- Script/Surge/Lxp/kekeyingyu.js
+++ Script/Surge/Lxp/kekeyingyu.js
@@ -2,11 +2,20 @@
  *脚本功能：可可英语VIP
  *脚本整理：Peng-Lx
  **********************
- *Surge&loon
- *[Script]
- *http-response ^https:\/\/mob2015\.kekenet\.com\/keke\/mobile\/index\.php requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/Lxp/kekeyingyu.js
+ *QX
+ [rewrite_local]
+ ^https:\/\/mob2015\.kekenet\.com\/keke\/mobile\/index\.php url script-response-body https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/Lxp/kekeyingyu.js
+ 
+ **********************
  *hostname = mob2015.kekenet.com
+ **********************
+ 
+ *Surge&loon
+ [Script]
+  http-response ^https:\/\/mob2015\.kekenet\.com\/keke\/mobile\/index\.php requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/PF2022/Peng-Lx/master/Script/Surge/Lxp/kekeyingyu.js
 */
+ 
+ 
  
  re('"is_vip":\\d@"end_time":\\d+','"is_vip":1@"end_time":1741575902')
 
